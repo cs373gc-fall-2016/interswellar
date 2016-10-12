d95c6a17d0ff3648d2dfd6cb19f6699778c91333
Nathan Poag <npoag@utexas.edu>
2016-10-12 16:05:59 -0500

    Redacted to original message

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return '<h1>Hello From Nathan!</h1>'
+    return '<h1>Hello World!</h1>'
 
 if __name__ == "__main__":
     application.debug = True
