3cecd27a42d5fa7eaccabff0a6f4778779da18a2
Sami Glasco <samiglasco@gmail.com>
2016-10-14 18:22:03 -0500

    set up index.html

--- interswellar/views.py
+++ interswellar/views.py
@@ -1,9 +1,10 @@
 """ The views for the app """
 
 from interswellar import app
-
+from flask import Flask, render_template
 
 @app.route('/')
 def index():
     """ Returns 'My God, it's full of stars!' """
-    return "<h1>My God, it\'s full of stars!</h1>"
+    # return "<h1>My God, it\'s full of stars!</h1>"
+    return render_template('index.html')
