47147304976023befbab927c0af99037385138ba
Sami Glasco <samiglasco@gmail.com>
2016-10-17 14:30:09 -0500

    fix unused import for pylint

--- interswellar/views.py
+++ interswellar/views.py
@@ -1,10 +1,10 @@
 """ The views for the app """
 
 from interswellar import app
-from flask import Flask, render_template
+from flask import render_template
+# from flask import Flask, render_template
 
 @app.route('/')
 def index():
-    """ Returns 'My God, it's full of stars!' """
-    # return "<h1>My God, it\'s full of stars!</h1>"
+    """ Returns splash page """
     return render_template('index.html')
