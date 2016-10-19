e83abc890deafd67cc3c6c971a3f60d133537a88
Sami Glasco <samiglasco@gmail.com>
2016-10-19 16:33:57 -0500

    pylint fixes for views.py

--- interswellar/views.py
+++ interswellar/views.py
@@ -1,9 +1,10 @@
 """ The views for the app """
-# pylint: disable=unused-import
+# pylint: disable=unused-import, bad-continuation, import-error
+from collections import defaultdict
+import json
+import requests
 from interswellar import app
 from flask import Flask, render_template
-import requests, json
-from collections import defaultdict
 
 
 @app.route('/')
@@ -17,34 +18,36 @@ def about():
     """ Returns about page with commit and issues count"""
     commits = get_commits()
     issues = get_issues()
-    return render_template('about.html', 
-    	charlotte_commits=commits.get('charharharhar', 0),
-    	sami_commits=commits['TheFireFerret'],
-    	denise_commits=commits.get('denisely',0),
-    	young_commits=commits['jedyobidan'],
-    	david_commits=commits['dshimo'],
-    	nathan_commits=commits['nazopo'],
-    	charlotte_issues=issues.get('charharharhar', 0),
-    	sami_issues=issues['TheFireFerret'],
-    	denise_issues=issues.get('denisely',0),
-    	young_issues=issues['jedyobidan'],
-    	david_issues=issues.get('dshimo',0),
-    	nathan_issues=issues['nazopo'])
+    return render_template('about.html',
+        charlotte_commits=commits.get('charharharhar', 0),
+        sami_commits=commits.get('TheFireFerret', 0),
+        denise_commits=commits.get('denisely', 0),
+        young_commits=commits.get('jedyobidan', 0),
+        david_commits=commits.get('dshimo', 0),
+        nathan_commits=commits.get('nazopo', 0),
+        charlotte_issues=issues.get('charharharhar', 0),
+        sami_issues=issues.get('TheFireFerret', 0),
+        denise_issues=issues.get('denisely', 0),
+        young_issues=issues.get('jedyobidan', 0),
+        david_issues=issues.get('dshimo', 0),
+        nathan_issues=issues.get('nazopo', 0))
+
 
 def get_commits():
-	url = 'https://api.github.com/repos/cs373gc-fall-2016/interswellar/stats/contributors'
-	response = requests.get(url).text
-	parsed = json.loads(response)
-	return {user['author']['login'] : user['total'] for user in parsed}
+    """ gets number of commits for each team member """
+    url = 'https://api.github.com/repos/cs373gc-fall-2016/interswellar/stats/contributors'
+    response = requests.get(url).text
+    parsed = json.loads(response)
+    return {user['author']['login'] : user['total'] for user in parsed}
+
 
 def get_issues():
-	url = 'https://api.github.com/repos/cs373gc-fall-2016/interswellar/issues?state=all'
-	response = requests.get(url).text
-	parsed = json.loads(response)
-	num_issues = defaultdict(int)
-	for issue in parsed:
-		if 'pull_request' not in issue:
-			num_issues[issue['user']['login']] += 1
-	return num_issues
-
-
+    """ get number of issues for each team member """
+    url = 'https://api.github.com/repos/cs373gc-fall-2016/interswellar/issues?state=all'
+    response = requests.get(url).text
+    parsed = json.loads(response)
+    num_issues = defaultdict(int)
+    for issue in parsed:
+        if 'pull_request' not in issue:
+            num_issues[issue['user']['login']] += 1
+    return num_issues
