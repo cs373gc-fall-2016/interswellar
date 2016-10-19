2673629b3cb98f6c43791827fc9342bc0b57676a
David Shi <dshi@utexas.edu>
2016-10-19 12:54:31 -0500

    fixed pylint complaints for travis

--- interswellar/models.py
+++ interswellar/models.py
@@ -1,11 +1,17 @@
+"""models for database"""
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+BASE = declarative_base()
 
-class Star(Base):
+# pylint: disable=too-few-public-methods
+class Star(BASE):
+    """model for stars"""
     __tablename__ = 'stars'
 
+    # pylint: disable=invalid-name
     id = Column(Integer, primary_key=True)
     name = Column(String)
     mass = Column(Float)
@@ -16,24 +22,26 @@ class Star(Base):
     # constellation, publication discovered
 
     def __repr__(self):
-        return "<Star(name='%s', mass=%f, luminosity=%f, temperature=%f, radius=%f)>" 
-            % (self.name, self.mass, self.luminosity, self.temperature, self.radius)
+        return "<Star(name='%s', mass=%f, luminosity=%f, temperature=%f, radius=%f)>" % (
+            self.name, self.mass, self.luminosity, self.temperature, self.radius)
 
-class Exoplanet(Base):
+# pylint: disable=too-few-public-methods
+class Exoplanet(BASE):
+    """model for exoplanets"""
     __tablename__ = 'exoplanets'
 
+    # pylint: disable=invalid-name
     id = Column(Integer, primary_key=True)
     name = Column(String)
     mass = Column(Float)
     radius = Column(Float)
     orbital_period = Column(Integer)
     year_discovered = Column(Integer)
-    star_id = Column(Integer, ForeignKey("star.id")))
+    star_id = Column(Integer, ForeignKey("star.id"))
     star = relationship("Star", back_populates="exoplanets")
     # constellation, publication discovered
 
     def __repr__(self):
-        return "<exoplanets(name='%s', mass=%f, radius=%f, orbital_period=%d, year_discovered=%d)>" 
-            % (self.name, self.mass, self.radius, self.orbital_period, self.year_discovered)
-
-
+        return "<Exoplanet(name='%s', mass=%f, radius=%f, orbital_period=%d, "  \
+            "year_discovered=%d)>" % (self.name, self.mass, self.radius,
+                                      self.orbital_period, self.year_discovered)
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
+format:
+	find . -type f \( -name "*.py" \) | xargs autopep8 -i
+
 lint:
 	find . -type f \( -name "*.py" -and -not -name "*_test.py" \) | xargs pylint -r n -d locally-disabled -d cyclic-import
 
